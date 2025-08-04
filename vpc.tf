555552328e3e7f3524bf06e86c3a8ba104caaf80
Arambakam-yaswanth <yyyashu@gmail.com>
2025-08-04 11:33:53 +0530

    lms-subnets

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,33 @@ resource "aws_vpc" "lms-vpc" {
     Name = "lms-vpc"
   }
 }
+# web subnet
+resource "aws_subnet" "lms-web-subnet" {
+  vpc_id     = aws_vpc.lms-vpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch ="true"
+
+  tags = {
+    Name = "Main"
+  }
+}
+# api subnet
+resource "aws_subnet" "lms-api-subnet" {
+  vpc_id     = aws_vpc.lms-vpc.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch ="true"
+
+  tags = {
+    Name = "Main"
+  }
+}
+# database subnet
+resource "aws_subnet" "lms-batabase-subnet" {
+  vpc_id     = aws_vpc.lms-vpc.id
+  cidr_block = "10.0.3.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
